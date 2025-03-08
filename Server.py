a985dcc42f2728f0d9500d08ef5cabe97c5f19dc
Wahaj Al Obid <whalobid@student.rtc.edu>
2025-03-07 16:02:16 -0800

    ping EC2 using python

--- Server.py
+++ Server.py
@@ -1,10 +1,15 @@
+import os
+import platform
+import subprocess
+
 class Server:
     """ Server class for representing and manipulating servers. """
 
     def __init__(self, server_ip):
+
         # TODO -
         self.server_ip = server_ip
-
     def ping(self):
         # TODO - Use os module to ping the server
-        return
+        response = os.system(f"ping -c 5 {self.server_ip}" if os.name != "nt" else f"ping -n 5 {self.server_ip}")
+        return response == 0
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # This is the template code for the CNE335 Final Project
 # Justin Ellis
 # CNE 335 Fall
-
+from Server import Server
 def print_program_info():
     # TODO - Change your name
     print("Server Automator v0.1 by Your Name")
@@ -9,5 +9,9 @@ def print_program_info():
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
+    server = Server("34.219.58.195")
+    is_reachable = server.ping()
+    print(f"Server {server.server_ip} is {'reachable' if is_reachable else 'not reachable'}.")
+
     # TODO - Create a Server object
     # TODO - Call Ping method and print the results
